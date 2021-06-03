4e9853416532e45fdce31d2bec60d58442ae0bb1
everywherewego <964353527@qq.com>
2021-06-03 19:25:30 +0800

    v4 done

--- 01.terminal/remote/start.sh
+++ 01.terminal/remote/start.sh
@@ -4,7 +4,7 @@ script_abs=$(readlink -f "$0")
 path=${script_abs%/bin*}
 
 echo $path
-java -Xms2048m -Xmx2048m -jar $path/${project.name}-1.0-SNAPSHOT.jar $@ 1>$path/log.log 2>$path/log.log &
+cd $path && java -Xms1024m -Xmx1024m -jar ${project.name}-1.0-SNAPSHOT.jar $@ 1>$path/log.log 2>$path/log.log &
 
 #启动监控脚本
 checkrunpid=$(ps -ef | grep checkrun.sh | grep -v grep | awk '{print $2}')
