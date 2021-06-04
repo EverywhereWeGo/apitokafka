7d4887af8bbc642f1b207c0f49ab7493dcd2895c
everywherewego <964353527@qq.com>
2021-06-04 16:36:42 +0800

    v5 done

--- 01.terminal/remote/start.sh
+++ 01.terminal/remote/start.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
+echo 开始启动程序
 script_abs=$(readlink -f "$0")
 path=${script_abs%/bin*}
 
-echo $path
 cd $path && java -Xms1024m -Xmx1024m -jar ${project.name}-1.0-SNAPSHOT.jar $@ 1>$path/log.log 2>$path/log.log &
 
+
+echo 开始启动监控脚本
 #启动监控脚本
 checkrunpid=$(ps -ef | grep checkrun.sh | grep -v grep | awk '{print $2}')
 kill -9 ${checkrunpid}
--- 01.terminal/remote/stop.sh
+++ 01.terminal/remote/stop.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+echo 开始关闭程序
 pid=`ps -ef | grep ${project.name}-1.0-SNAPSHOT.jar | grep -v grep | awk '{print $2}'`
 echo $pid
 kill -9 $pid
+
+echo 开始关闭监控程序
+checkrunpid=$(ps -ef | grep checkrun.sh | grep -v grep | awk '{print $2}')
+kill -9 ${checkrunpid}
